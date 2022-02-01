dc5a375c3e6ceb8bf3fc83ef6598055a026eba94
Zixail28 <bezgubov.04@yandex.ru>
2022-02-01 20:07:12 +0300

    docs: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    let str = n.toString()
+    let strN = ''
+    for(i = str.length - 1; i >= 0; i--){
+        strN = `${strN}${str[i]}`
+    } return strN
 }
